63448941ec1c9a2e94cb751276164588eef04420
DanyloKalinchuk <kalinchukdanylo@gmail.com>
2026-04-17 16:31:04 +0200

    Implemented IPC logic

--- src/master/scada/ipc/ipc.hpp
+++ src/master/scada/ipc/ipc.hpp
@@ -7,22 +7,36 @@
 #include <unistd.h>
 #include <chrono>
 #include <map>
+#include <vector>
 #include <utility>
 #include <cstdint>
 #include <thread>
 #include <atomic>
 #include <stdexcept>
 
+#define MSG_CONF (1)
+#define MSG_REQ (2)
+
+#define CONF_BUFF_SIZE (8)
+
 #define SOCKET_PATH ("/tmp/SCADA_SOCK")
 
 class IPC{
     std::thread ipc_thread;
     std::atomic<bool> ipc_on;
+    
+    EnvControl env_control;
 
     int sfd, cfd;
     struct sockaddr_un addr;
 
+    void ipc_write(uint16_t* msg);
+    uint16_t ipc_read();
+
+    protected:
     void ipc_handling();
+    virtual void handle_configuration(std::vector<uint16_t> params);
+    virtual void handle_data_request();
 
     public:
     IPC();
