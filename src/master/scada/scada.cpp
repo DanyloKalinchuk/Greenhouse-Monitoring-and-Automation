8f104e894d98c40842b85030b5ae7acccdd99f60
DanyloKalinchuk <kalinchukdanylo@gmail.com>
2026-04-21 21:10:26 +0200

    Updated main loop

--- src/master/scada/scada.cpp
+++ src/master/scada/scada.cpp
@@ -1,9 +1,13 @@
 #include "ipc/ipc.hpp"
+#include <thread>
+#include <chrono>
 
 int main(){
     IPC ipc = IPC();
 
-    while (1);
+    while(1){
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
 
     return 0;
 }
