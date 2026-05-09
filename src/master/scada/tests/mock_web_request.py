89adcdab717d3bc12cb533b8a7d9dbe17cf68d13
DanyloKalinchuk <kalinchukdanylo@gmail.com>
2026-05-09 12:15:30 +0200

    Fixed web_ipc module import issues

--- src/master/scada/tests/mock_web_request.py
+++ src/master/scada/tests/mock_web_request.py
@@ -1,4 +1,10 @@
-from  web_ipc import *
+import sys
+import os
+
+REL_PATH = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(REL_PATH, "../../web"))
+
+from web_ipc import IPC, SensFrame
 
 SOCKET_PATH = "/tmp/SCADA_SOCK"
 ipc = IPC(SOCKET_PATH)
